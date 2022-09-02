1a1a44cbf3838b5eb2806526f3262253e2aef6a1
Aditya Wibisana <aditya.wibisana@gmail.com>
2022-09-02 21:05:30 +0700

    Update search text when author is clicked

--- lib/presentation/page/book_detail.dart
+++ lib/presentation/page/book_detail.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../business_logic/cubit/search_cubit.dart';
 import '../../data/model/book.dart';
 
 class BookDetail extends StatelessWidget {
@@ -19,6 +21,7 @@ class BookDetail extends StatelessWidget {
             InkWell(
               child: Text(book.authors.isNotEmpty ? book.authors[0].name : ""),
               onTap: () => {
+                context.read<SearchCubit>().updateSearch(book.authors[0].name),
                 Navigator.of(context).pop(),
               },
             ),
--- lib/presentation/page/home.dart
+++ lib/presentation/page/home.dart
@@ -4,12 +4,14 @@ import 'package:gutendex/business_logic/paging_feature.dart';
 import 'package:gutendex/presentation/widget/infinite_book_list.dart';
 
 import '../../business_logic/cubit/search_cubit.dart';
+import '../../business_logic/cubit/search_state.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final searchTextController = TextEditingController();
     return Material(
       child: SafeArea(
         child: MultiRepositoryProvider(
@@ -31,6 +33,17 @@ class Home extends StatelessWidget {
                 onSubmitted: (s) => {
                   context.read<SearchCubit>().search(s),
                 },
+                controller: searchTextController,
+              ),
+              BlocListener<SearchCubit, SearchState>(
+                listener: (context, state) {
+                  if (state is SearchTypingCompleted) {
+                    searchTextController.text = state.query;
+                  } else {
+                    searchTextController.text = "";
+                  }
+                },
+                child: const SizedBox.shrink(),
               ),
               const Expanded(child: InfiniteBookList()),
             ],
